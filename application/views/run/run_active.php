fb84975f9c237c4fe8656f4ec70757fd2bad89cc
cindylu165 <cindylu242@gmail.com>
2021-06-30 10:18:42 +0800

    change-variable

--- application/views/run/run_active.php
+++ application/views/run/run_active.php
@@ -17,49 +17,48 @@
 </div>
 <div class="container">
     <div class="row">
-    <h4 class="text-dark text-center"><?php echo $title ?></h4>
+    <!-- <h4 class="text-dark text-center"><?php echo $title ?></h4> -->
         <form action="<?php echo site_url($url); ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
             <div class="col-10 m-2 mx-auto">
-                <label for="id" class="form-label">活動名稱</label>
-                <input class="form-control" type="text" id="id" name="id" value="暨大春健" required placeholder="請輸入活動名稱">
+                <label for="runName" class="form-label">活動名稱</label>
+                <input class="form-control" type="text" id="runName" name="runName" value="暨大春健" required placeholder="請輸入活動名稱">
             </div>
             <div class="row justify-content-center">
                 <div class="col-10 m-2 mx-auto" id="chineseDiv">
                 <label for="formDate">活動日期</label>
-                <input type="text" id="dateRun" class="form-control" value="2021/06/01" required placeholder="請輸入活動日期">
+                <input type="text" id="dateRun" class="form-control" value="2021-06-01" required placeholder="請輸入活動日期">
                 </div>
             </div>
             <div class="row justify-content-center">
                 <div class="col-sm-10 col-md-3">
-                <input type="text" id="hiddenRun" name="date" class="form-control" style="display:none" 
-                                value=""/>
+                <input type="text" id="hiddenRun" name="date" class="form-control" style="display:none" value=""/>
                 </div>
             </div>
             <div class="col-10 m-2 mx-auto">
-                <label for="id" class="form-label">活動地點</label>
-                <input class="form-control" type="text" id="id" name="id" value="國立暨南大學" required placeholder="請輸入活動名稱">
+                <label for="place" class="form-label">活動地點</label>
+                <input class="form-control" type="text" id="place" name="place" value="國立暨南大學" required placeholder="請輸入活動名稱">
             </div> 
             <!-- startTime -->
             <div class="row justify-content-center" id="chineseDiv">
-            <div class="col-md-5">
-              <label for="formStartTime">開始報名時間(日期)*</label>
-              <!-- <input class="form-control" type="text" id="dateFrom" value="<?php echo (empty($date_start)) ? "" : $date_start ?>"> -->
-              <input class="form-control" type="text" id="dateFrom" >
+                <div class="col-md-5">
+                <label for="formStartTime">開始報名時間(日期)*</label>
+                <!-- <input class="form-control" type="text" id="dateFrom" value="<?php echo (empty($date_start)) ? "" : $date_start ?>"> -->
+                <input class="form-control" type="text" id="dateFrom" >
+                </div>
+                <div class="col-md-5">
+                <label for="formStartTime">開始報名時間(時間)*</label>
+                <!-- <input class="form-control time-picker-start" type="text" id="formStartTime" value="<?php echo (empty($time_start)) ? "" : $time_start ?>"> -->
+                <input class="form-control time-picker-start" type="text" id="formStartTime">
+                </div>
             </div>
-            <div class="col-md-5">
-              <label for="formStartTime">開始報名時間(時間)*</label>
-              <!-- <input class="form-control time-picker-start" type="text" id="formStartTime" value="<?php echo (empty($time_start)) ? "" : $time_start ?>"> -->
-              <input class="form-control time-picker-start" type="text" id="formStartTime">
+            <div class="row justify-content-center">
+                <div class="col-md-10" style="display:none">
+                <!-- <input type="text" name="date" id="hiddenFrom" class="form-control" value="<?php echo(empty($startDate)) ? "" : $startDate?>"/> -->
+                <!-- <input type="text" id="startTime" name="startTime" class="form-control"/> -->
+                <input type="text" name="date" id="hiddenFrom" class="form-control"/>
+                <input type="text" id="startTime" name="startTime" class="form-control"/>
+                </div>
             </div>
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-md-10" style="display:none">
-              <!-- <input type="text" name="date" id="hiddenFrom" class="form-control" value="<?php echo(empty($startDate)) ? "" : $startDate?>"/> -->
-              <!-- <input type="text" id="startTime" name="startTime" class="form-control"/> -->
-              <input type="text" name="date" id="hiddenFrom" class="form-control"/>
-              <input type="text" id="startTime" name="startTime" class="form-control"/>
-            </div>
-          </div>
 <br>
           <!-- endTime -->
           <div class="row justify-content-center">
