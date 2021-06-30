84a4ae2f9671aa853aaaefab4ad3d8227f9d1da2
cindylu165 <cindylu242@gmail.com>
2021-06-30 14:46:07 +0800

    add function pass-point and pass-point_table

--- application/controllers/Run.php
+++ application/controllers/Run.php
@@ -124,7 +124,7 @@ class Run extends CI_Controller
             redirect('user/login');
         }
     }
-    public function pass_point()
+    public function pass_point_table($no = null)
     {
         $passport = $this->session->userdata('passport');
         $userTitle = $passport['userTitle'];
@@ -133,7 +133,28 @@ class Run extends CI_Controller
         if (in_array($current_role, $accept_role)) {
             $beSentDataset = array(
                 'title' => '路跑經過點',
-                'url' => '/run/pass_point/',
+                'url' => '/run/pass_point/'.$no,
+                'role' => $current_role,
+                'userTitle' => $userTitle,
+                'current_role' => $current_role,
+                'password' => $passport['password']
+            );
+
+            $this->load->view('/run/pass_point_table', $beSentDataset);
+        } else {
+            redirect('user/login');
+        }
+    }
+    public function pass_point($no = null)
+    {
+        $passport = $this->session->userdata('passport');
+        $userTitle = $passport['userTitle'];
+        $current_role = $passport['role'];
+        $accept_role = array(6);
+        if (in_array($current_role, $accept_role)) {
+            $beSentDataset = array(
+                'title' => '路跑經過點表單',
+                'url' => '/run/pass_point/'.$no,
                 'role' => $current_role,
                 'userTitle' => $userTitle,
                 'current_role' => $current_role,
