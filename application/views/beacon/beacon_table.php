77e90d26f6ad2cd5b307ed1d2e544ba30504ce82
cindylu165 <cindylu242@gmail.com>
2021-07-01 10:09:20 +0800

    add content

--- application/views/beacon/beacon_table.php
+++ application/views/beacon/beacon_table.php
@@ -12,6 +12,37 @@
     </ol>
   </nav>
 </div>
-<div class="container">
+<div class="container" style="width:95%">
+  <div class="row">
+    <div class="d-grid gap-2 col-2 mx-auto">
+      <a class="btn btn-info m-3" href="<?php echo site_url($url); ?>">新增</a><br>
+    </div>
+    <table class="table text-center border-secondary table-hover align-middle">
+      <thead class="header" style="background-color:#C8C6A7">
+        <tr>
+          <th scope="col">Beacon編號</th>
+          <th scope="col">Beacon型號</th>
+          <th scope="col">是否可使用</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="col">asd123</th>
+          <td scope="col">Ci - BLE MESH</td>
+          <td scope="col"><a type="button" class="btn btn-warning" href="<?php echo site_url('run/pass_point/1' );?>">是</a></td>
+        </tr>
+        <tr>
+          <th scope="col">qwe123</th>
+          <td scope="col">Ci - AYU MESH</td>
+          <td scope="col"><a type="button" class="btn btn-warning" href="<?php echo site_url('run/pass_point/2' );?>">是</a></td>
+        </tr>
+        <tr>
+          <th scope="col">qwe126</th>
+          <td scope="col">Ci - IOP MESH</td>
+          <td scope="col"><a type="button" class="btn btn-warning" href="<?php echo site_url('run/pass_point/3' );?>">是</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div> 
 </div>
 <?php $this->load->view('templates/new_footer');?>
