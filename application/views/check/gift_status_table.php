0dfff338b4e2c5c2478520f7bd89262dba96bd51
107213049 <kyse1003@gmail.com>
2021-07-02 10:07:32 +0800

    redeem_gift UI ok

--- application/views/check/gift_status_table.php
+++ application/views/check/gift_status_table.php
@@ -13,5 +13,44 @@
   </nav>
 </div>
 <div class="container">
+  <br>
+  <table class="table text-center border-secondary table-hover align-middle">
+    <thead class="header" style="background-color:#C8C6A7">
+      <tr>
+        <th scope="col">會員編號</th>
+        <th scope="col">姓名</th>
+        <th scope="col">參加之路跑編號</th>
+        <th scope="col">組別</th>
+        <th scope="col">要項</th>
+        <th scope="col">連絡電話</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="col">M000001</th>
+        <td scope="col">會員依</td>
+        <td scope="col">A1</td>
+        <td scope="col">休閒組</td>
+        <td scope="col">已領取</td>
+        <td scope="col">0900000000</td>
+      </tr>
+      <tr>
+        <th scope="col">M000002</th>
+        <td scope="col">會員貳</td>
+        <td scope="col">A1</td>
+        <td scope="col">休閒組</td>
+        <td scope="col">已領取</td>
+        <td scope="col">0900000001</td>
+      </tr>
+      <tr>
+        <th scope="col">M000003</th>
+        <td scope="col">會員參</td>
+        <td scope="col">A1</td>
+        <td scope="col">挑戰組</td>
+        <td scope="col">尚未領取</td>
+        <td scope="col">0900000002</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 <?php $this->load->view('templates/new_footer');?>
