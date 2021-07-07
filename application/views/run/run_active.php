21f762fb73c76f46f65e6f729a90af0c75b21fc2
cindylu165 <cindylu242@gmail.com>
2021-07-07 16:33:20 +0800

    update with function content down

--- application/views/run/run_active.php
+++ application/views/run/run_active.php
@@ -17,67 +17,72 @@
 </div>
 <div class="container">
     <div class="row">
-    <!-- <h4 class="text-dark text-center"><?php echo $title ?></h4> -->
-        <form action="<?php echo site_url($url); ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
-        <?php foreach($activity as $i){
+      <form action="<?php echo site_url($url); ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+      <input type="hidden" name="<?php echo $security->get_csrf_token_name() ?>" value="<?php echo $security->get_csrf_hash() ?>" />
+      
+      <?php echo isset($error) ? '<p class="red-text text-darken-3 text-center">' . $error . '</p>' : ''; ?>
+      <?php echo isset($success) ? '<p class="green-text text-darken-3 text-center">' . $success . '</p>' : ''; ?>
             
-        ?>
             <div class="col-10 m-2 mx-auto">
                 <label for="runName" class="form-label">活動名稱</label>
-                <input class="form-control" type="text" id="runName" name="runName" value="<?php echo $i['name'];?>" required placeholder="請輸入活動名稱">
+                <input class="form-control" type="text" id="runName" name="runName" value="<?php echo (empty($activity)) ? "" : $activity->name ?>" placeholder="請輸入活動名稱">
             </div>
             <div class="row justify-content-center">
                 <div class="col-10 m-2 mx-auto" id="chineseDiv">
                 <label for="formDate">活動日期</label>
-                <input type="text" id="dateRun" class="form-control" value="<?php echo $i['date'];?>" required placeholder="請輸入活動日期">
+                <input type="text" id="dateRun" name="dateRun" class="form-control" value="<?php echo (empty($activity)) ? "" : $activity->date ?>" placeholder="請輸入活動日期">
                 </div>
             </div>
             <div class="col-10 m-2 mx-auto">
                 <label for="place" class="form-label">活動地點</label>
-                <input class="form-control" type="text" id="place" name="place" value="國立暨南大學" required placeholder="請輸入活動名稱">
+                <input class="form-control" type="text" id="place" name="place" value="國立暨南大學" placeholder="請輸入活動名稱">
             </div> 
             <!-- startTime -->
             <div class="row justify-content-center" id="chineseDiv">
                 <div class="col-md-5">
-                <label for="formStartTime">開始報名時間(日期)*</label>
-                <!-- <input class="form-control" type="text" id="dateFrom" value="<?php echo (empty($date_start)) ? "" : $date_start ?>"> -->
-                <input class="form-control" type="text" id="dateFrom" name="startDate">
+                  <label for="formStartTime">開始報名時間(日期)*</label>
+                  <input class="form-control" type="text" id="dateFrom"  name="startDate" value="<?php echo (empty($activity)) ? "" : substr($activity->start_time, 0, 10) ?>">
                 </div>
                 <div class="col-md-5">
-                <label for="formStartTime">開始報名時間(時間)*</label>
-                <!-- <input class="form-control time-picker-start" type="text" id="formStartTime" value="<?php echo (empty($time_start)) ? "" : $time_start ?>"> -->
-                <input class="form-control time-picker-start" type="text" id="formStartTime" name="startTime">
+                  <label for="formStartTime">開始報名時間(時間)*</label>
+                  <input class="form-control time-picker-start" type="text" id="formStartTime" name="startTime" value="<?php echo (empty($activity)) ? "" : substr($activity->start_time, 11, strlen($activity->start_time)) ?>">
                 </div>
             </div>
           <!-- endTime -->
           <div class="row justify-content-center">
             <div class="col-md-5">
               <label for="formEndTime">結束報名時間(日期)*</label>
-              <input class="form-control" type="text" id="dateTo" name="endDate">
+              <input class="form-control" type="text" id="dateTo" name="endDate" value="<?php echo (empty($activity)) ? "" : $activity->date ?>">
             </div>
             <div class="col-md-5">
               <label for="formEndTime">結束報名時間(時間)*</label>
-              <input class="form-control time-picker-end" type="text" id="formEndTime" name="endTime">
+              <input class="form-control time-picker-end" type="text" id="formEndTime" name="endTime" value="<?php echo (empty($activity)) ? "" : $activity->date ?>">
             </div>
           </div>
           <div class="col-10 m-2 mx-auto">
                 <label for="bankCode" class="form-label">銀行代號</label>
-                <input class="form-control" type="text" id="bankCode" name="bankCode" value="" required placeholder="請輸入銀行代號">
+                <input class="form-control" type="text" id="bankCode" name="bankCode" value="<?php echo (empty($activity)) ? "" : $activity->bank_code ?>" placeholder="請輸入銀行代號">
             </div> 
           <div class="col-10 m-2 mx-auto">
               <label for="bankAccount" class="form-label">銀行帳號</label>
-              <input class="form-control" type="text" id="bankAccount" name="bankAccount" value="" required placeholder="請輸入銀行帳號">
+              <input class="form-control" type="text" id="bankAccount" name="bankAccount" value="<?php echo (empty($activity)) ? "" : $activity->bank_account ?>" placeholder="****-****-****-****">
           </div> 
           <div class="col-10 m-2 mx-auto">
-            <label for="photoFile">上傳圖片</label>
-            <input type="file" id="photoFile" name="photoFile" class="form-control" value="" required >
+            <label for="photoFile">上傳圖片(jpg/png/pdf)</label>
+            <input type="file" id="photoFile" name="photoFile" class="form-control" >
           </div>
+          <?php if (!empty($activity)): ?>
+          <div class="col-10 m-2 mx-auto">
+              <img class="img-fluid" style="width:250px"
+                src="<?php echo site_url() . '/files/photo/' . $activity->photo_name; ?>" />
+            </div>
+          <?php endif; ?>
+
           <div class="row">
             <div class="d-grid gap-2 col-2 mx-auto">
               <button class="btn btn-primary m-3" type="submit">送出</button>
             </div>
           </div>
-          <?php }?>
         </form>
     </div>
     <br><br><br><br><br><br><br>
