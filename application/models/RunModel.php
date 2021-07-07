e202558b68d4e2eb8675f95929bccffa62eff788
cindylu165 <cindylu242@gmail.com>
2021-07-07 16:32:24 +0800

    update get_active_by_id and add create_one-and-update_by_id

--- application/models/RunModel.php
+++ application/models/RunModel.php
@@ -8,8 +8,38 @@ class RunModel extends CI_Model
     }
     public function get_active_by_id($runNo)
     {
+        $this->db->select('running_activity.*,f.name as photo_name,f.no as no');
         $this->db->where('running_ID', $runNo);
-        $result = $this->db->get('running_activity',1)->result_array();
-        return $result;
+        $this->db->join('files as f', 'running_activity.file_no = f.no', 'left');
+        $result = $this->db->get('running_activity',1);
+        return $result->row();
+    }
+    function create_one($id,$name, $date,$place,$start_time,$end_time,$bankCode,$bankAccount,$file_no) {
+
+        $this->running_ID  = $id;
+        $this->name = $name;
+        $this->date = $date;
+        $this->place = $place;
+        $this->start_time = $start_time;
+        $this->end_time = $end_time;
+        $this->bank_account = $bankAccount;
+        $this->bank_code = $bankCode;
+        $this->file_no = $file_no;
+      
+        return ($this->db->insert('running_activity', $this)) ? $this->db->insert_id() : '';
+    }
+    function update_by_id($id,$name, $date,$place,$start_time,$end_time,$bankCode,$bankAccount,$file_no) {
+
+        $this->name = $name;
+        $this->date = $date;
+        $this->place = $place;
+        $this->start_time = $start_time;
+        $this->end_time = $end_time;
+        $this->bank_account = $bankAccount;
+        $this->bank_code = $bankCode;
+        $this->file_no = $file_no;
+        
+        $this->db->where('running_ID', $id);
+        return $this->db->update('running_activity', $this);
     }
 }
