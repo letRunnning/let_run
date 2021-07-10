81906d861e4ef597861e81c183871fd41047dacc
cindylu165 <cindylu242@gmail.com>
2021-07-10 23:21:55 +0800

    update insert column

--- application/models/FileModel.php
+++ application/models/FileModel.php
@@ -4,8 +4,7 @@ class FileModel extends CI_Model {
    * create one file
    * @Return: file auto generated no
    */
-  function create_one($owner, $name, $originalName) {
-    $this->owner = $owner;
+  function create_one($name, $originalName) {
     $this->name = $name;
     $this->original_name = $originalName;
     return ($this->db->insert('files', $this)) ?  $this->db->insert_id() : false;
