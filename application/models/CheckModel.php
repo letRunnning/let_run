98b2f88c850e1de14fb30a69498e3a9d74718821
107213049 <kyse1003@gmail.com>
2021-07-21 17:26:27 +0800

    fix member_pay function

--- application/models/CheckModel.php
+++ application/models/CheckModel.php
@@ -3,11 +3,11 @@ class CheckModel extends CI_Model
 {
     public function get_all_member_pay()
     {
-        $this->db->select('transaction.member_ID as mID, transaction.time, member.name, member.email, registration.running_ID, registration.group_name');
-        $this->db->join('registration', 'transaction.registration_ID = registration.registration_ID');
-        $this->db->join('member', 'transaction.member_ID = member.member_ID');
-        $this->db->order_by('transaction.registration_ID', 'asc');
-        $result = $this->db->get('transaction') -> result_array();
+        $this->db->select('registration.member_ID as mID, transaction.time, member.name, member.email, registration.running_ID, registration.group_name');
+        $this->db->join('transaction', 'transaction.registration_ID = registration.registration_ID', 'left');
+        $this->db->join('member', 'registration.member_ID = member.member_ID');
+        $this->db->order_by('registration.registration_ID', 'asc');
+        $result = $this->db->get('registration') -> result_array();
         return $result;
     }
 }
