0d6cd8c547626992f0c0832cf7bf3f0fd9bef115
cindylu165 <cindylu242@gmail.com>
2021-08-03 11:37:08 +0800

    update

--- api/updateInfo_member.php
+++ api/updateInfo_member.php
@@ -16,26 +16,44 @@
         $contact_phone = $data[0]['Contact_phone'];
         $relation = $data[0]['Relation'];
         $Photo_code = $data[0]['Photo_code'];
-        if($Photo_code){
-            
-        }
+        $uploadcode = $data[0]['Uploadcode'];
 
-        if($name != '' && $id_card !='' && $password !='' && $email !='' && $phone!=''){
-          $sql = "UPDATE `member` SET `id_card`='$id_card',`name`='$name',`phone`='$phone',
-          `email`='$email',`birthday`='$birthday',`address`='$address',`contact_name`='$contact_name',
-          `contact_phone`='$contact_phone',`relation`='$relation',`file_no`='$file_no' WHERE `member_ID` = '$member_ID'";
-          $result = mysqli_query($con, $sql) ;//or die("Insert failed, SQL query error,sql"); //執行SQL       
-          if($result){
-              echo json_encode(["ans" => "yes"]);
-          }else{
-              echo json_encode(["ans" => "no"]);
-          }
+        $file_no ='';
+        if($uploadcode){
+            $imageName = "25220_".date("His",time())."_".rand(1111,9999).'.png';
+            if (strstr($uploadcode,",")){
+                $uploadcode = explode(',',$uploadcode);
+                $uploadcode = $uploadcode[1];
+            }
+            $path = "../photo/";
+            $imageSrc=  $path."/". $imageName; 
+            $r = file_put_contents($imageSrc, base64_decode($uploadcode));//返回的是位元組數
+            if ($r) {
+                $sql = "INSERT INTO `files`(`no`, `name`, `original_name`, `path`, `create_time`, `usable`) VALUES (null,'$imageName','$imageName','https://letrun05.000webhostapp.com/photo',now(),1);";
+                $result = mysqli_query($con, $sql) ;
+                $file_no = mysqli_insert_id($con);
+            }
+        }
+        if($file_no != ''){
+            if($name != '' && $id_card !='' && $password !='' && $email !='' && $phone!=''){
+                $sql = "UPDATE `member` SET `id_card`='$id_card',`name`='$name',`phone`='$phone',
+            `email`='$email',`birthday`='$birthday',`address`='$address',`contact_name`='$contact_name',
+            `contact_phone`='$contact_phone',`relation`='$relation',`file_no`='$file_no' WHERE `member_ID` = '$member_ID'";
+                $result = mysqli_query($con, $sql) ;//or die("Insert failed, SQL query error,sql"); //執行SQL       
+                if($result){
+                    echo json_encode(["ans" => "yes"]);
+                }else{
+                    echo json_encode(["ans" => "no"]);
+                }
+            }else{
+                echo json_encode(["ans" => "No empty"]);
+            }
         }else{
-              echo json_encode(["ans" => "No empty"]);
-          }
-      } else {
-          echo json_encode(["ans" => "DataBase connection failed"]);
-          //echo "DataBase connection failed";
-      }
+            echo json_encode(["ans" => "photo upload failed"]);
+        }
+    }else {
+        echo json_encode(["ans" => "DataBase connection failed"]);
+        //echo "DataBase connection failed";
+    }
 
 ?>
