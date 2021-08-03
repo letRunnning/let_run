285c7e8351e002005124581cf67dc74914922983
cindylu165 <cindylu242@gmail.com>
2021-08-03 19:09:58 +0800

    update enroll_m numbers

--- api/enroll_member.php
+++ api/enroll_member.php
@@ -14,24 +14,31 @@
         $contact_name = $data[0]['Contact_name'];
         $contact_phone = $data[0]['Contact_phone'];
         $relation = $data[0]['Relation'];
-        
-        $sql_num = "SELECT COUNT(*) as num FROM `member` WHERE 1 LIMIT 1";
+
+        $pwd_hash = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql_num = "SELECT * FROM `member` ORDER BY `member`.`member_ID` DESC LIMIT 1";
         $nums=mysqli_query($con,$sql_num) or die("DB Error: Cannot retrieve message.");
         $nums = mysqli_fetch_assoc($nums);
-        $member_ID = 'M'.sprintf("%06d",$nums['num']+1);
+        $sNums = substr($nums['member_ID'],1,strlen($nums['member_ID']));
+        $member_ID = 'M'.sprintf("%06d",$sNums+1);
 
         if($name != '' && $id_card !='' && $password !='' && $email !='' && $phone!=''){
-          $sql = "INSERT INTO `member`(`member_ID`, `id_card`, `name`, `phone`, `email`, `birthday`, `password`, `address`, `contact_name`, `contact_phone`, `relation`, `file_no`) 
-          VALUES ('$member_ID','$id_card','$name','$phone','$email','$birthday','$password','$address','$contact_name','$contact_phone','$relation',null);";
-          $result = mysqli_query($con, $sql) ;//or die("Insert failed, SQL query error,sql"); //執行SQL       
-          if($result){
-              echo json_encode(["ans" => "$member_ID"]);
-          }else{
-              echo json_encode(["ans" => "NoSuccess"]);
-          }
+            if (preg_match("/[0-9]+/", $password) && preg_match("/[a-z]+/", $password) && preg_match("/[A-Z]+/", $password) && strlen($password) >= 8) {
+                $sql = "INSERT INTO `member`(`member_ID`, `id_card`, `name`, `phone`, `email`, `birthday`, `password`, `address`, `contact_name`, `contact_phone`, `relation`, `file_no`) 
+                VALUES ('$member_ID','$id_card','$name','$phone','$email','$birthday','$pwd_hash','$address','$contact_name','$contact_phone','$relation',null);";
+                $result = mysqli_query($con, $sql) ;//or die("Insert failed, SQL query error,sql"); //執行SQL       
+                if($result){
+                    echo json_encode(["ans" => "$member_ID"]);
+                }else{
+                    echo json_encode(["ans" => "NoSuccess"]);
+                }
+            }else{
+                echo urldecode(json_encode(["ans" => urlencode("密碼需包含英文字母大寫、英文字母小寫與數字並長度大於8")]));
+            }
         }else{
               echo json_encode(["ans" => "NoEmpty"]);
-          }
+        }
       } else {
           echo json_encode(["ans" => "DataBase connection failed"]);
           //echo "DataBase connection failed";
