e1a206209218fe348cb0d77c2fdcf4b6136da987
cindylu165 <cindylu242@gmail.com>
2021-08-12 11:25:16 +0800

    update

--- api/activeRecord_detail_member.php
+++ api/activeRecord_detail_member.php
@@ -4,22 +4,26 @@
     if ($con) {
         header("Content-Type: application/json; charset=UTF-8");
         $data = json_decode(file_get_contents("php://input"), true);
-        $run_ID = $data[0]['running_ID'];
-        $regis_ID = $data[0]['registration_ID'];
+        $run_ID = $data[0]['Running_ID'];
+        $regis_ID = $data[0]['Registration_ID'];
         // $run_ID = 'A1';
-        // $regis_ID = 'A1M000001';
+        // $regis_ID = 'A1M000004';
         
         if($run_ID != ''&& $regis_ID !=''){
-            $sql_id = "SELECT * FROM `registration` JOIN `running_activity` ON `registration`.`running_ID` = `running_activity`.`running_ID`
+            $sql_id="SELECT `running_activity`.`name`as runName,registration.group_name as groupName
+            ,`running_group`.`place` as groupPlace,running_group.time as groupTime FROM `registration` 
+            JOIN `running_group`ON `running_group`.`running_ID`=`registration`.`running_ID` 
+            AND `running_group`.`group_name`=`registration`.`group_name` 
+            JOIN `running_activity` ON `registration`.`running_ID`= `running_activity`.`running_ID` 
             WHERE `registration_ID` = '$regis_ID' and `registration`.`running_ID`='$run_ID'";
             $result_qrcode = mysqli_query($con, $sql_id) or die("DB Error: Cannot retrieve message.");
             $data = array();
             foreach ($result_qrcode as $i){
                 $array = array( 
-                    'name' => urlencode($i['name']),
-                    'group_name' => urlencode($i['group_name']),
-                    'place' => urlencode($i['place']),
-                    'time' => $i['time']
+                    'Name' => urlencode($i['runName']),
+                    'Group_name' => urlencode($i['groupName']),
+                    'Place' => urlencode($i['groupPlace']),
+                    'Time' => $i['groupTime']
                 );
                 array_push($data, $array);
             }
