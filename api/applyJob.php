4008a86ab239f395a8b5f82c182e1083d21df78a
107213049 <kyse1003@gmail.com>
2021-08-13 14:37:35 +0800

    Complete apply_job API

---
+++ api/applyJob.php
@@ -0,0 +1,36 @@
+<?php
+    require("staffModel.php");
+    header('Content-Type: application/json; charset=UTF-8');
+
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if ($data) {
+            $participation = check_application($data[0]['staff_ID'], $data[0]['running_ID']);
+            $row = mysqli_fetch_assoc($participation);
+
+            if ($row == null) {
+                $number = check_application_number($data[0]['workgroup_ID'], $data[0]['running_ID']);
+                $row2 = mysqli_fetch_assoc($number);
+                $maxNum = get_maximum_number($data[0]['workgroup_ID'], $data[0]['running_ID']);
+                $row3 = mysqli_fetch_assoc($maxNum);
+                
+                if ($row2['COUNT(`staff_ID`)'] == $row3['maximum_number']) {
+                    echo json_encode(["ans" => "ToTheMaximumNumber"]);
+                } else {
+                    $apply = apply_job($data[0]['workgroup_ID'], $data[0]['staff_ID'], $data[0]['running_ID']);
+                    $result = check_application_update($data[0]['staff_ID'], $data[0]['workgroup_ID'], $data[0]['running_ID']);
+                    $row4 = mysqli_fetch_assoc($result);
+
+                    if ($row4) {
+                        echo json_encode(["ans" => "yes"]);
+                    } else {
+                        echo json_encode(["ans" => "no"]);
+                    }
+                }
+            } else {
+                echo json_encode(["ans" => $row['name']]);
+            }
+        } else {
+            echo json_encode(["ans" => "No data sent"]);
+        }
+?>
--- api/staffModel.php
+++ api/staffModel.php
@@ -43,4 +43,59 @@
         
         return $result;
     }
+
+    function check_application($sid, $rid) {
+        global $db;
+        $sql = "SELECT `name` FROM `staff_participation` JOIN `work_group` ON `work_group`.`workgroup_ID` = `staff_participation`.`workgroup_ID` WHERE `staff_ID` = ? AND `staff_participation`.`running_ID` = ?";
+        $stmt = mysqli_prepare($db, $sql); // prepare sql statement
+        mysqli_stmt_bind_param($stmt, "ss", $sid, $rid); // bind parameters with variables
+        mysqli_stmt_execute($stmt); // 執行 SQL
+        $result = mysqli_stmt_get_result($stmt); // get result
+        
+        return $result;
+    }
+
+    function get_maximum_number($wid, $rid) {
+        global $db;
+        $sql = "SELECT `maximum_number` FROM `work_group` WHERE `workgroup_ID` = ? AND `running_ID` = ?";
+        $stmt = mysqli_prepare($db, $sql); // prepare sql statement
+        mysqli_stmt_bind_param($stmt, "is", $wid, $rid); // bind parameters with variables
+        mysqli_stmt_execute($stmt); // 執行 SQL
+        $result = mysqli_stmt_get_result($stmt); // get result
+        
+        return $result;
+    }
+
+    function check_application_number($wid, $rid) {
+        global $db;
+        $sql = "SELECT COUNT(`staff_ID`) FROM `staff_participation` WHERE `workgroup_ID` = ? AND `running_ID` = ?";
+        $stmt = mysqli_prepare($db, $sql); // prepare sql statement
+        mysqli_stmt_bind_param($stmt, "is", $wid, $rid); // bind parameters with variables
+        mysqli_stmt_execute($stmt); // 執行 SQL
+        $result = mysqli_stmt_get_result($stmt); // get result
+        
+        return $result;
+    }
+
+    function apply_job($wid, $sid, $rid) {
+        global $db;
+        $sql = "INSERT INTO `staff_participation`(`workgroup_ID`, `staff_ID`, `running_ID`) VALUES (?,?,?)";
+        $stmt = mysqli_prepare($db, $sql); // prepare sql statement
+        mysqli_stmt_bind_param($stmt, "iss", $wid, $sid, $rid); // bind parameters with variables
+        mysqli_stmt_execute($stmt); // 執行 SQL
+        $result = mysqli_stmt_get_result($stmt); // get result
+        
+        return $result;
+    }
+
+    function check_application_update($sid, $wid, $rid) {
+        global $db;
+        $sql = "SELECT * FROM `staff_participation` WHERE `staff_ID` = ? AND `workgroup_ID` = ? AND `running_ID` = ?";
+        $stmt = mysqli_prepare($db, $sql); // prepare sql statement
+        mysqli_stmt_bind_param($stmt, "sis", $sid, $wid, $rid); // bind parameters with variables
+        mysqli_stmt_execute($stmt); // 執行 SQL
+        $result = mysqli_stmt_get_result($stmt); // get result
+        
+        return $result;
+    }
 ?>
