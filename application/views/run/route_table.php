1dedfae8f9434b3b059857ae97339010e62d36b0
cindylu165 <cindylu242@gmail.com>
2021-08-23 17:45:45 +0800

    update with update db,finish

--- application/views/run/route_table.php
+++ application/views/run/route_table.php
@@ -18,59 +18,19 @@
         <a class="btn btn-info m-3" href="<?php echo site_url($url); ?>">新增</a>
         </div>
     <div>
+    <?php foreach($groups as $i => $value){ ?>
     <div class="card">
-        <div class="card-header" id="headingOne">
-        <div class="row">
-            <div class="col-6 text-left"><span class="fs-5">路線1</span></div>
-            <div class="col-2 text-right"><span class="fs-5">暨大春健</span></div>
-            <div class="col text-right"><span class="fs-5">休閒組</span></div>
-            <div class="col text-right"><span class="fs-5 text-primary collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">查看</span></div>
-            <div class="col text-right"><a class="fs-5 text-primary " type="button" href="<?php echo site_url('run/route/1' );?>">編輯</a></div>
-        </div>
-        </div>
-
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-        <div class="card-body">
-            <span class=''>大草原 -> </span><span>暨大體育館 -> </span>
-            <span class=''>活動中心 -> </span><span>科院 -> </span>
-            <span class=''>教院 -> </span><span>大草原</span><br>
-        </div>
+        <div class="card-header" id="<?php echo "heading".$i?>">
+            <div class="row">
+                <div class="col-6 text-left"><span class="fs-5"><?php echo "路線".($i+1)?></span></div>
+                <div class="col-2 text-right"><span class="fs-5"><?php echo $value['name']?></span></div>
+                <div class="col text-right"><span class="fs-5"><?php echo $value['group_name']?></span></div>
+                <div class="col text-right"><a class="fs-5 text-primary " type="button" href="<?php echo site_url('run/route/'.$value['running_ID'].'/'.base64_encode($value['group_name']) );?>">查看/新增</a></div>
+                <div class="col text-right"><a class="fs-5 text-primary " type="button" href="<?php echo site_url('run/routeEdit/'.$value['running_ID'].'/'.base64_encode($value['group_name']) );?>">編輯</a></div>
+            </div>
         </div>
     </div>
-    <div class="card">
-        <div class="card-header" id="headingTwo">
-        <div class="row">
-            <div class="col-6 text-left"><span class="fs-5">路線2</span></div>
-            <div class="col-2 text-right"><span class="fs-5">暨大春健</span></div>
-            <div class="col text-right"><span class="fs-5">挑戰組</span></div>
-            <div class="col text-right"><span class="fs-5 text-primary collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">查看</span></div>
-            <div class="col text-right"><a class="fs-5 text-primary " type="button" href="<?php echo site_url('run/route/2' );?>">編輯</a></div>
-        </div>
-        </div>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-        <div class="card-body">
-            <span class=''>活動中心 -> </span><span>暨大體育館 -> </span>
-            <span class=''>大草原 -> </span><span>管院 -> </span>
-            <span class=''>科院 -> </span><span>行政大樓 -> </span>
-            <span class=''>人院 -> </span><span>活動中心</span><br>  
-        </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header" id="headingThree">
-        <div class="row">
-            <div class="col-6 text-left"><span class="fs-5">路線3</span></div>
-            <div class="col-2 text-right"><span class="fs-5">台中花博馬拉松</span></div>
-            <div class="col text-right"><span class="fs-5">菁英組</span></div>
-            <div class="col text-right"><span class="fs-5 text-primary collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">查看</span></div>
-            <div class="col text-right"><a class="fs-5 text-primary " type="button" href="<?php echo site_url('run/route/3' );?>">編輯</a></div>
-        </div>
-        </div>
-        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-        <div class="card-body">
-            <span class=''>台中市政府廣場 -> </span><span>台中花博廣場</span><br>
-        </div>
-    </div>
+    <?php }?>
     </div>
   </div> 
   </div> 
