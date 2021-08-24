c1b7e754139e5951f452e6098294624e6251a9a8
cindylu165 <cindylu242@gmail.com>
2021-08-24 23:31:18 +0800

    add group_name condition

--- api/mapRoutedetail.php
+++ api/mapRoutedetail.php
@@ -5,10 +5,16 @@
         header("Content-Type: application/json; charset=UTF-8");
         $data = json_decode(file_get_contents("php://input"), true);
         $running_ID = $data[0]['Running_ID'];
+        $m_ID = $data[0]['Member_ID'];
         // $running_ID = 'A1';
+        // $m_ID = 'M000004';
 
-        if($running_ID != ''){
-            $sql_id = "SELECT * FROM `route_detail` WHERE `running_ID` = '$running_ID'";
+        if($running_ID != '' && $m_ID != ''){
+            $sql_group = "SELECT * FROM `registration` 
+            WHERE `member_ID`= '$m_ID' and `running_ID` = '$running_ID' limit 1";
+            $tmp = mysqli_fetch_assoc(mysqli_query($con,$sql_group));
+            $group_name = $tmp['group_name'];
+            $sql_id = "SELECT * FROM `route_detail` WHERE `running_ID` = '$running_ID' and `group_name` = '$group_name'";
             $result_routes = mysqli_query($con, $sql_id) or die("DB Error: Cannot retrieve message.");
             $data = array();
             foreach ($result_routes as $i){
