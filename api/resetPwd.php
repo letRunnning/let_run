11364f7ba3e444dab2c2e4a0ac16d96336160813
cindylu165 <cindylu242@gmail.com>
2021-08-27 11:57:54 +0800

    new file to reset gorget pwd

---
+++ api/resetPwd.php
@@ -0,0 +1,29 @@
+<?php
+    $con = mysqli_connect("localhost", "id17256516_root", '&qM$(wP3Nu6/U$T3', "id17256516_run");
+    
+    if ($con) {
+        header("Content-Type: application/json; charset=UTF-8");
+        $data = json_decode(file_get_contents("php://input"), true);
+        $m_ID = $data[0]['Email'];
+        $password = $data[0]['Password'];
+
+        if($m_ID != '' && $password !=''){
+            if (preg_match("/[0-9]+/", $password) && preg_match("/[a-z]+/", $password) && preg_match("/[A-Z]+/", $password) && strlen($password) >= 8) {
+                $pwd_hash = password_hash($password, PASSWORD_DEFAULT);
+                $sql = "UPDATE `member` SET `password`='$pwd_hash' WHERE `email`='$m_ID'";
+                $result = mysqli_query($con, $sql) ;
+                if($result){
+                    echo json_encode([["ans" => "yes"]]);
+                }else{
+                    echo json_encode([["ans" => "no"]]);
+                }
+            }else{
+                echo urldecode(json_encode([["ans" => urlencode("密碼需包含英文字母大寫、英文字母小寫與數字並長度大於8")]]));
+            }
+        }else{
+            echo json_encode([["ans" => "Email and pwd can't empty"]]);
+        }    
+    } else {
+        echo json_encode([["ans" => "DataBase connection failed"]]);
+    }
+?>
