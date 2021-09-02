5b05da0757098138d29bb7cc9c8ab1a1ca5efacf
107213049 <kyse1003@gmail.com>
2021-09-02 20:42:46 +0800

    Add registration check

--- api/getBeacon.php
+++ api/getBeacon.php
@@ -9,6 +9,7 @@
 
             $now = date("Y-m-d");
             $activity = get_today_activity($now);
+            $registration = get_registration($data[0]['member_ID']);
 
             $i = 0;
             while ($row = mysqli_fetch_assoc($activity)) {
@@ -18,45 +19,51 @@
                 $i++;
             }
 
-            if ($response == null) {
-                $array = array(
-                    'running_ID' => null,
-                    'group_name' => null,
-                    'beaconList' => [],
-                    'ifCheckin' => 'NoActivity'
-                );
-                array_push($data2, $array);
-                echo urldecode(json_encode($data2, JSON_PRETTY_PRINT));
-            } else {
-                for ($i = 0; $i < count($response); $i++) {
-                    $beacon = get_beacon($response[$i]['running_ID']);
-                    $registrationID = $response[$i]['running_ID'].$data[0]['member_ID'];
-                    $checkin = get_checkin_by_mid($registrationID);
-                    $row2 = mysqli_fetch_assoc($checkin);
+            $row2 = mysqli_fetch_assoc($registration);
 
-                    $beaconList = array();
-
-                    foreach ($beacon as $j) {
-                        array_push($beaconList, $j['beacon_ID']);
-                    }
-
-                    if ($row2) {
-                        $response[$i]['ifCheckin'] = 'AlreadyChenkedIn';
-                    } else {
-                        $response[$i]['ifCheckin'] = 'NotYetChenkedIn';
-                    }
-
+            if ($row2) {
+                if ($response == null) {
                     $array = array(
-                        'running_ID' => $response[$i]['running_ID'],
-                        'group_name' => urlencode($response[$i]['name']),
-                        'beaconList' => $beaconList,
-                        'ifCheckin' => $response[$i]['ifCheckin']
+                        'running_ID' => null,
+                        'group_name' => null,
+                        'beaconList' => [],
+                        'ifCheckin' => 'NoActivity'
                     );
                     array_push($data2, $array);
+                    echo urldecode(json_encode($data2, JSON_PRETTY_PRINT));
+                } else {
+                    for ($i = 0; $i < count($response); $i++) {
+                        $beacon = get_beacon($response[$i]['running_ID']);
+                        $registrationID = $response[$i]['running_ID'].$data[0]['member_ID'];
+                        $checkin = get_checkin_by_mid($registrationID);
+                        $row2 = mysqli_fetch_assoc($checkin);
+
+                        $beaconList = array();
+
+                        foreach ($beacon as $j) {
+                            array_push($beaconList, $j['beacon_ID']);
+                        }
+
+                        if ($row2) {
+                            $response[$i]['ifCheckin'] = 'AlreadyChenkedIn';
+                        } else {
+                            $response[$i]['ifCheckin'] = 'NotYetChenkedIn';
+                        }
+
+                        $array = array(
+                            'running_ID' => $response[$i]['running_ID'],
+                            'group_name' => urlencode($response[$i]['name']),
+                            'beaconList' => $beaconList,
+                            'ifCheckin' => $response[$i]['ifCheckin']
+                        );
+                        array_push($data2, $array);
+                    }
+                    echo urldecode(json_encode($data2, JSON_PRETTY_PRINT));
                 }
-                echo urldecode(json_encode($data2, JSON_PRETTY_PRINT));
+            } else{
+                echo json_encode(array(["ifCheckin" => "No registration data"]));
             }
         } else {
-            echo json_encode(["ans" => "No data sent"]);
+            echo json_encode(array(["ans" => "No data sent"]));
         }
 ?>
--- api/memberModel.php
+++ api/memberModel.php
@@ -241,4 +241,15 @@
         
         return $result;
     }
+
+    function get_registration($mid) {
+        global $db;
+        $sql = "SELECT `registration_ID` FROM `registration` WHERE `member_ID` = ?";
+        $stmt = mysqli_prepare($db, $sql); // prepare sql statement
+        mysqli_stmt_bind_param($stmt, "s", $mid); // bind parameters with variables
+        mysqli_stmt_execute($stmt); // 執行 SQL
+        $result = mysqli_stmt_get_result($stmt); // get result
+        
+        return $result;
+    }
 ?>
