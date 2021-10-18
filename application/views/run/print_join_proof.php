4be4aef953b556fddb0acfa2f09197699c08a170
cindylu165 <cindylu242@gmail.com>
2021-10-18 09:44:37 +0800

    temp not yet

--- application/views/run/print_join_proof.php
+++ application/views/run/print_join_proof.php
@@ -14,7 +14,7 @@
 </div>
 
 <div class="container">
-  <div class="row justify-content-center">
+  <div class="row justify-content-center ">
     <div class="col-md-3">
       <label for="runActive" style="text-align:right;" class="col-form-label">搜尋</label>
       <input id="myInput" class="form-control" type="search" onkeyup="myFunction('all_counselor')" placeholder="搜尋路跑活動">
@@ -23,6 +23,13 @@
     <div class="col-md-3">
       <label for="memberName" style="text-align:right;" class="col-form-label">搜尋</label>
       <input id="myInput" class="form-control" type="search" onkeyup="myFunction('all_counselor')" placeholder="搜尋參賽者姓名">
+    </div>
+  </div>
+  <div class="row mt-5 justify-content-center" style="height: 100px;">
+    <div class="col-md-8 " style="background-color:white;border:2px black solid;">
+      <input class="form-control"value="證明">
+      <span class="justify-content-center">參賽證明</span><br>
+      
     </div>
   </div>
 </div>
