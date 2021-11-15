2934f5aa1151a840f433aa39b651274da4cd36c0
cindylu165 <cindylu242@gmail.com>
2021-11-15 09:21:09 +0800

    adjust back

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -1,68 +1,74 @@
 <?php
-    require_once("dbconnect.php");
+    $con = mysqli_connect("localhost", "pmauser", 'safety#25508', "run");
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     require $_SERVER['DOCUMENT_ROOT'] . '/PHPMailers/Exception.php';
     require $_SERVER['DOCUMENT_ROOT'] . '/PHPMailers/PHPMailer.php';
     require $_SERVER['DOCUMENT_ROOT'] . '/PHPMailers/SMTP.php';
     
+    if ($con) {
     header("Content-Type: application/json; charset=UTF-8");
+    $data = json_decode(file_get_contents("php://input"), true);
+    // $m_ID = $data[0]['Member_ID'];
     $m_ID="M000004";
-    if($m_ID!=''){
-        $temp = rand(1111111,9999999);
-        $sql = "UPDATE `member` SET `captcha`='$temp' WHERE `member_ID`='$m_ID'";
-        $result = mysqli_query($db, $sql) ;
-        if($result){
-            $name = 'service';
-            $email = 'letrun05@gmail.com';
-            $subject = '【No1路跑服務有限公司】忘記密碼-驗證碼通知信';
-            $body = '<h3>'.$m_ID.'您好，</h3><sapn>歡迎您加入No1路跑服務有限公司，您的驗證碼為：<span style="font-weight: bold">'.$temp.'</span></sapn><br>' . date('Y-m-d H:i:s');
+        if($m_ID!=''){
+            $temp = rand(1111111,9999999);
+            $sql = "UPDATE `member` SET `captcha`='$temp' WHERE `member_ID`='$m_ID'";
+            $result = mysqli_query($con, $sql) ;
+            if($result){
+                $name = 'service';
+                $email = 'letrun05@gmail.com';
+                $subject = '【No1路跑服務有限公司】忘記密碼-驗證碼通知信';
+                $body = '<h3>'.$m_ID.'您好，</h3><sapn>歡迎您加入No1路跑服務有限公司，您的驗證碼為：<span style="font-weight: bold">'.$temp.'</span></sapn><br>' . date('Y-m-d H:i:s');
 
-            // require_once "PHPMailer/PHPMailer.php";
-            // require_once "PHPMailer/SMTP.php";
-            // require_once "PHPMailer/Exception.php";
+                // require_once "PHPMailer/PHPMailer.php";
+                // require_once "PHPMailer/SMTP.php";
+                // require_once "PHPMailer/Exception.php";
 
-            $mail = new PHPMailer();
+                $mail = new PHPMailer();
 
-            //SMTP Settings
-            $mail->isSMTP();
-            // $mail->SMTPDebug = 2;
-            $mail->Host = "smtp.gmail.com";
-            $mail->SMTPAuth = true;
-            $mail->Username = "letrun05@gmail.com";
-            $mail->Password = 'letrunrun05';
-            $mail->Port = 587; //587
-            $mail->SMTPSecure = "tls"; //tls
+                //SMTP Settings
+                $mail->isSMTP();
+                // $mail->SMTPDebug = 2;
+                $mail->Host = "smtp.gmail.com";
+                $mail->SMTPAuth = true;
+                $mail->Username = "letrun05@gmail.com";
+                $mail->Password = 'letrunrun05';
+                $mail->Port = 587; //587
+                $mail->SMTPSecure = "tls"; //tls
 
-            //Email Settings
-            $mail->isHTML(true);
-            $mail->setFrom($email, $name);
-            $mail->addAddress($email);
-            $mail->Subject = $subject;
-            $mail->Body = $body;
-            $mail->SMTPOptions = array(
-                'ssl' => array(
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                    'allow_self_signed' => true
-                )
-            );
+                //Email Settings
+                $mail->isHTML(true);
+                $mail->setFrom($email, $name);
+                $mail->addAddress($email);
+                $mail->Subject = $subject;
+                $mail->Body = $body;
+                $mail->SMTPOptions = array(
+                    'ssl' => array(
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true
+                    )
+                );
+                
+
+                if ($mail->send()) {
+                    $status = "success";
+                    $response = "Email is sent!".$subject;
+                } else {
+                    $status = "failed";
+                    $response = "Something is wrong: <br><br>" . $mail->ErrorInfo;
+                }
+
+                echo (json_encode(array("status" => $status, "response" => $response)));
             
-
-            if ($mail->send()) {
-                $status = "success";
-                $response = "Email is sent!".$subject;
-            } else {
-                $status = "failed";
-                $response = "Something is wrong: <br><br>" . $mail->ErrorInfo;
+            }else{
+                echo json_encode(["ans" => "NoSuccess"]);
             }
-
-            echo (json_encode(array("status" => $status, "response" => $response)));
-        
         }else{
-            echo json_encode(["ans" => "NoSuccess"]);
+            echo json_encode(["ans" => "Member_ID can't empty!"]);
         }
-    }else{
-        echo json_encode(["ans" => "Member_ID can't empty!"]);
+    } else {
+        echo json_encode(["ans" => "DataBase connection failed"]);
     }
 ?>
