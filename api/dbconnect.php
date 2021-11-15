51926a59440d93673d4555dd3f644943c68fd53f
cindylu165 <cindylu242@gmail.com>
2021-11-15 09:17:11 +0800

    change to server linkdata

--- api/dbconnect.php
+++ api/dbconnect.php
@@ -3,9 +3,9 @@
 連線資料庫用的副程式
 */
 $host = 'localhost'; // 執行 DB Server 的主機
-$user = 'id17256516_root'; // 登入 DB 用的 DB 帳號
-$pass = '&qM$(wP3Nu6/U$T3'; // 登入 DB 用的 DB 密碼
-$dbName = 'id17256516_run'; // 使用的資料庫名稱
+$user = 'pmauser'; // 登入 DB 用的 DB 帳號
+$pass = 'safety#25508'; // 登入 DB 用的 DB 密碼
+$dbName = 'run'; // 使用的資料庫名稱
 
 /* $db 即為未來執行 SQL 指令所使用的物件 */
 $db = mysqli_connect($host, $user, $pass, $dbName) or die('Error with MySQL connection'); // 跟 MyMSQL 連線並登入
