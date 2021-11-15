9f1864b4f0f6a7dcb0d755e02a83ceffdd80f8db
cindylu165 <cindylu242@gmail.com>
2021-11-15 20:06:47 +0800

    page done

--- application/views/run/rungroup_gift_table.php
+++ application/views/run/rungroup_gift_table.php
@@ -21,40 +21,25 @@
     <table class="table text-center border-secondary table-hover align-middle">
       <thead class="header" style="background-color:#C8C6A7">
         <tr>
-          <th scope="col">路跑ID</th>
+          <th scope="col">路跑名稱</th>
           <th scope="col">組別名稱</th>
           <th scope="col">報到地點</th>
           <th scope="col">起跑時間</th>
-          <th scope="col">報到時間</th>
+          <th scope="col">結束時間</th>
           <th scope="col">要項</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="col">A1</th>
-          <td scope="col">休閒組</td>
-          <td scope="col">暨大大草原</td>
-          <td scope="col">2021/06/01 07:00</td>
-          <td scope="col">2021/06/01 06:30</td>
-          <!-- <td scope="col"><button type="button" class="btn btn-warning" href="<?php echo site_url('run/rungroup_gift/' .$i['no']);?>">編輯/查看</button></td> -->
-          <td scope="col"><a type="button" class="btn btn-warning" href="<?php echo site_url('run/rungroup_gift/1' );?>">編輯/查看</a></td>
-        </tr>
-        <tr>
-          <th scope="col">A1</th>
-          <td scope="col">挑戰組</td>
-          <td scope="col">暨大大草原</td>
-          <td scope="col">2021/06/01 06:30</td>
-          <td scope="col">2021/06/01 06:00</td>
-          <td scope="col"><a type="button" class="btn btn-warning" href="<?php echo site_url('run/rungroup_gift/2' );?>">編輯/查看</a></td>
-        </tr>
-        <tr>
-          <th scope="col">A3</th>
-          <td scope="col">菁英組</td>
-          <td scope="col">埔里鎮鎮公所</td>
-          <td scope="col">2021/06/15 07:00</td>
-          <td scope="col">2021/05/15 06:30</td>
-          <td scope="col"><a type="button" class="btn btn-warning" href="<?php echo site_url('run/rungroup_gift/3' );?>">編輯/查看</a></td>
-        </tr>
+      <?php foreach($rungroups as $i) { ?>
+      <tr>
+        <th scope="col"><?php echo $i['runName']?></th>
+        <th scope="col"><?php echo $i['group_name']?></th>
+        <th scope="col"><?php echo $i['time']?></th>
+        <th scope="col"><?php echo $i['start_time']?></th>
+        <th scope="col"><?php echo $i['end_time']?></th>
+        <td scope="col"><a type="button" class="btn btn-warning" href="<?php echo site_url('run/rungroup_gift/'.$i['running_ID'].'/'.base64_encode($i['group_name'])  );?>">編輯/查看</a></td>
+      </tr>
+      <?php } ?>
       </tbody>
     </table>
   </div> 
