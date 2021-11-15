1a3168a51b472181e2569e0f347a6e2f8ed34b9a
cindylu165 <cindylu242@gmail.com>
2021-11-15 09:20:13 +0800

    adjust pwd if-else

--- api/resetPwd.php
+++ api/resetPwd.php
@@ -2,13 +2,13 @@
     require_once("dbconnect.php");
     header("Content-Type: application/json; charset=UTF-8");
     $data = json_decode(file_get_contents("php://input"), true);
-    $m_ID = $data[0]['Email'];
+    $email = $data[0]['Email'];
     $password = $data[0]['Password'];
 
-    if($m_ID != '' && $password !=''){
-        if (preg_match("/[0-9]+/", $password) && preg_match("/[a-z]+/", $password) && preg_match("/[A-Z]+/", $password) && strlen($password) >= 8) {
+    if($email != '' && $password !=''){
+        if (preg_match("/[0-9]+/", $password) && (preg_match("/[a-z]+/", $password) || preg_match("/[A-Z]+/", $password)) && strlen($password) >= 8) {
             $pwd_hash = password_hash($password, PASSWORD_DEFAULT);
-            $sql = "UPDATE `member` SET `password`='$pwd_hash' WHERE `email`='$m_ID'";
+            $sql = "UPDATE `member` SET `password`='$pwd_hash' WHERE `email`='$email'";
             $result = mysqli_query($db, $sql) ;
             if($result){
                 echo json_encode([["ans" => "yes"]]);
@@ -16,7 +16,7 @@
                 echo json_encode([["ans" => "no"]]);
             }
         }else{
-            echo urldecode(json_encode([["ans" => urlencode("密碼需包含英文字母大寫、英文字母小寫與數字並長度大於8")]]));
+            echo urldecode(json_encode([["ans" => urlencode("密碼需包含英文字母與數字並長度大於8")]]));
         }
     }else{
         echo json_encode([["ans" => "Email and pwd can't empty"]]);
