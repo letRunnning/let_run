69e3fa4b6ca211c8a8df26ce76e99b581e633e45
cindylu165 <cindylu242@gmail.com>
2021-11-15 20:03:58 +0800

    add path

--- application/models/FileModel.php
+++ application/models/FileModel.php
@@ -4,9 +4,10 @@ class FileModel extends CI_Model {
    * create one file
    * @Return: file auto generated no
    */
-  function create_one($name, $originalName) {
+  function create_one($name, $originalName,$path) {
     $this->name = $name;
     $this->original_name = $originalName;
+    $this->path = $path;
     return ($this->db->insert('files', $this)) ?  $this->db->insert_id() : false;
   }
 }
