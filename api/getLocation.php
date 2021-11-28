e7d4d330981c1c615b0d359d13fc4095d2496f72
cindylu165 <cindylu242@gmail.com>
2021-11-28 16:49:32 +0800

    1128done

--- api/getLocation.php
+++ api/getLocation.php
@@ -1,22 +1,29 @@
 <?php
     require("memberModel.php");
     header('Content-Type: application/json; charset=UTF-8');
-    
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $rid = $_POST['running_ID'];
-        $data = json_decode(file_get_contents("php://input"), true);
-        $result = get_location($rid);
 
-        $i = 0;
-        $response = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            $response[$i]['beacon_ID'] = $row['beacon_ID'];
-            $response[$i]['num'] = $row['num'];
-            $i++;
-        }
+    if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        $rid = $_GET['running_ID'];
 
-        if ($response) {
-            echo urldecode(json_encode($response, JSON_PRETTY_PRINT));
+        if ($rid) {
+            $location = get_location($rid);
+
+            $i = 0;
+            $response = array();
+            while ($row = mysqli_fetch_assoc($location)) {
+                $response[$i]['beacon_ID'] = $row['beacon_ID'];
+                $response[$i]['num'] = $row['num'];
+                $i++;
+            }
+
+            if ($response) {
+                echo urldecode(json_encode(["running_ID" => $rid, "number" => $response]));
+            } else {
+                echo "0";
+            }
+            // echo $rid;
+        } else {
+            echo urldecode(json_encode(["ans" => "NoData"]));
         }
     }
 ?>
