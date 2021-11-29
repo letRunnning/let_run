3befbe0613d2e123c7676610c0f136014332d369
cindylu165 <cindylu242@gmail.com>
2021-11-29 19:09:57 +0800

    add new func run_qrcode

--- application/controllers/Prints.php
+++ application/controllers/Prints.php
@@ -22,7 +22,7 @@ class Prints extends CI_Controller
         $qrcodes = $runNo ? $this->PrintsModel->get_beacons_by_id($runNo) : null;
         if($qrcodes!= null){
             require_once 'phpqrcode/qrlib.php';
-            $path = 'files/qrcode/';
+            $path = './files/qrcode/';
             foreach($qrcodes as $re){
                 $name = base64_encode($re['beacon_ID']);
                 $file = $path.$name.".png";
@@ -48,6 +48,44 @@ class Prints extends CI_Controller
             redirect('user/login');
         }
     }
+    public function run_qrcode($runNo = null)
+    {
+        $passport = $this->session->userdata('passport');
+        $userTitle = $passport['userTitle'];
+        $current_role = $passport['role'];
+        $accept_role = array(6);
+        $runID = $runNo ? $runNo : null;
+        $activities = $this->RunModel->get_all_active();
+        $activity = $runNo ? $this->RunModel->get_active_by_id($runNo) : null;
+        $qrcodes = $runNo ? $this->PrintsModel->get_beacons_by_id($runNo) : null;
+        if($runNo!= null){
+            require_once 'phpqrcode/qrlib.php';
+            $path = './files/qrcode/';
+            // foreach($qrcodes as $re){
+            $name = base64_encode($runNo);
+            $file = $path.$name.".png";
+            QRcode::png($runNo, $file); // 產生qrcode，並存入image資料夾下
+            // }
+        }
+        if (in_array($current_role, $accept_role)) {
+            $beSentDataset = array(
+                'title' => '列印路跑活動 Qrcode',
+                'url' => '/prints/run_qrcode/',
+                'role' => $current_role,
+                'userTitle' => $userTitle,
+                'current_role' => $current_role,
+                'password' => $passport['password'],
+                'activities' => $activities,
+                'activity' => $activity,
+                'runID' => $runID,
+                'qrcodes' => $qrcodes
+            );
+
+            $this->load->view('/prints/run_qrcode', $beSentDataset);
+        } else {
+            redirect('user/login');
+        }
+    }
     public function supply_qrcode($runNo = null)
     {
         $passport = $this->session->userdata('passport');
@@ -60,7 +98,7 @@ class Prints extends CI_Controller
         $supplys = $runNo ? $this->PrintsModel->get_supplys_by_id($runNo) : null;
         if($supplys!= null){
             require_once 'phpqrcode/qrlib.php';
-            $path = 'files/qrcode/';
+            $path = './files/qrcode/';
             foreach($supplys as $re){
                 $name = base64_encode($re['supply_ID']);
                 $file = $path.$name.".png";
